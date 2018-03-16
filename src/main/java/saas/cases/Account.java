53b9e7ee809a065405b2109ca1b5a40279615230
linavaos <839071557@qq.com>
2018-03-16 21:34:54 +0800

    1.添加账户对象

---
+++ src/main/java/saas/cases/Account.java
@@ -0,0 +1,28 @@
+package saas.cases;
+
+public class Account {
+	
+	private Double amount;
+	private String id;
+	private String type;
+	
+	public Double getAmount() {
+		return amount;
+	}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
