2af45781ee0330d428e8dee94bd744a405eb519e
wuzhen <wuzhen@zhoupu123.com>
2018-03-16 18:25:11 +0800

    1.添加账户对象,明细对象

---
+++ src/main/java/saas/cases/Account.java
@@ -0,0 +1,29 @@
+package saas.cases;
+
+public class Account {
+	
+	private double amount;
+	private String id;
+	private String type;
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+
+}
--- src/main/java/saas/cases/Detail.java
+++ src/main/java/saas/cases/Detail.java
@@ -2,4 +2,238 @@ package saas.cases;
 
 public class Detail {
 
+	private	String	articleNumber;
+	private	String	goodsName;
+	private	String	goodsId;
+	private	String	barcode	;
+	//生产日期是否开启0没开启,1开启
+	private	String	productionDateState;
+	private	String	currUnitId;
+	private	String	currUnitName;
+	private	String	currUnitFactor;
+	private	Double	origPrice;
+	private	Double	realPrice;
+	private	Double	costPrice;
+	private	String	currUnitFactorName;
+	private	String	productionDate;
+	private	String	baseUnitId;
+	private	String	baseUnitName;
+	private	Double	baseUnitFactor;
+	private	String	baseBarcode;
+	private	Double	basePurchase;
+	private	String	pkgUnitId;
+	private	String	pkgUnitName;
+	private	Double	pkgUnitFactor;
+	private	String	pkgBarcode;
+	private	Double	pkgPurchase;
+	private	String	unitFactorName;
+	private	String	stockArray;
+	private	Double	stockQuantity;
+	private	Double	reserveQuantity;
+	private	Double	availableQuantity;
+	//realprice
+	private	String	editField;
+	private	Double	quantity;
+	private	Double subAmount;
+	private	Double	discount;
+	private	String	seq;
+	public String getArticleNumber() {
+		return articleNumber;
+	}
+	public void setArticleNumber(String articleNumber) {
+		this.articleNumber = articleNumber;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public String getGoodsId() {
+		return goodsId;
+	}
+	public void setGoodsId(String goodsId) {
+		this.goodsId = goodsId;
+	}
+	public String getBarcode() {
+		return barcode;
+	}
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+	public String getProductionDateState() {
+		return productionDateState;
+	}
+	public void setProductionDateState(String productionDateState) {
+		this.productionDateState = productionDateState;
+	}
+	public String getCurrUnitId() {
+		return currUnitId;
+	}
+	public void setCurrUnitId(String currUnitId) {
+		this.currUnitId = currUnitId;
+	}
+	public String getCurrUnitName() {
+		return currUnitName;
+	}
+	public void setCurrUnitName(String currUnitName) {
+		this.currUnitName = currUnitName;
+	}
+	public String getCurrUnitFactor() {
+		return currUnitFactor;
+	}
+	public void setCurrUnitFactor(String currUnitFactor) {
+		this.currUnitFactor = currUnitFactor;
+	}
+	public Double getOrigPrice() {
+		return origPrice;
+	}
+	public void setOrigPrice(Double origPrice) {
+		this.origPrice = origPrice;
+	}
+	public Double getRealPrice() {
+		return realPrice;
+	}
+	public void setRealPrice(Double realPrice) {
+		this.realPrice = realPrice;
+	}
+	public Double getCostPrice() {
+		return costPrice;
+	}
+	public void setCostPrice(Double costPrice) {
+		this.costPrice = costPrice;
+	}
+	public String getCurrUnitFactorName() {
+		return currUnitFactorName;
+	}
+	public void setCurrUnitFactorName(String currUnitFactorName) {
+		this.currUnitFactorName = currUnitFactorName;
+	}
+	public String getProductionDate() {
+		return productionDate;
+	}
+	public void setProductionDate(String productionDate) {
+		this.productionDate = productionDate;
+	}
+	public String getBaseUnitId() {
+		return baseUnitId;
+	}
+	public void setBaseUnitId(String baseUnitId) {
+		this.baseUnitId = baseUnitId;
+	}
+	public String getBaseUnitName() {
+		return baseUnitName;
+	}
+	public void setBaseUnitName(String baseUnitName) {
+		this.baseUnitName = baseUnitName;
+	}
+	public Double getBaseUnitFactor() {
+		return baseUnitFactor;
+	}
+	public void setBaseUnitFactor(Double baseUnitFactor) {
+		this.baseUnitFactor = baseUnitFactor;
+	}
+	public String getBaseBarcode() {
+		return baseBarcode;
+	}
+	public void setBaseBarcode(String baseBarcode) {
+		this.baseBarcode = baseBarcode;
+	}
+	public Double getBasePurchase() {
+		return basePurchase;
+	}
+	public void setBasePurchase(Double basePurchase) {
+		this.basePurchase = basePurchase;
+	}
+	public String getPkgUnitId() {
+		return pkgUnitId;
+	}
+	public void setPkgUnitId(String pkgUnitId) {
+		this.pkgUnitId = pkgUnitId;
+	}
+	public String getPkgUnitName() {
+		return pkgUnitName;
+	}
+	public void setPkgUnitName(String pkgUnitName) {
+		this.pkgUnitName = pkgUnitName;
+	}
+	public Double getPkgUnitFactor() {
+		return pkgUnitFactor;
+	}
+	public void setPkgUnitFactor(Double pkgUnitFactor) {
+		this.pkgUnitFactor = pkgUnitFactor;
+	}
+	public String getPkgBarcode() {
+		return pkgBarcode;
+	}
+	public void setPkgBarcode(String pkgBarcode) {
+		this.pkgBarcode = pkgBarcode;
+	}
+	public Double getPkgPurchase() {
+		return pkgPurchase;
+	}
+	public void setPkgPurchase(Double pkgPurchase) {
+		this.pkgPurchase = pkgPurchase;
+	}
+	public String getUnitFactorName() {
+		return unitFactorName;
+	}
+	public void setUnitFactorName(String unitFactorName) {
+		this.unitFactorName = unitFactorName;
+	}
+	public String getStockArray() {
+		return stockArray;
+	}
+	public void setStockArray(String stockArray) {
+		this.stockArray = stockArray;
+	}
+	public Double getStockQuantity() {
+		return stockQuantity;
+	}
+	public void setStockQuantity(Double stockQuantity) {
+		this.stockQuantity = stockQuantity;
+	}
+	public Double getReserveQuantity() {
+		return reserveQuantity;
+	}
+	public void setReserveQuantity(Double reserveQuantity) {
+		this.reserveQuantity = reserveQuantity;
+	}
+	public Double getAvailableQuantity() {
+		return availableQuantity;
+	}
+	public void setAvailableQuantity(Double availableQuantity) {
+		this.availableQuantity = availableQuantity;
+	}
+	public String getEditField() {
+		return editField;
+	}
+	public void setEditField(String editField) {
+		this.editField = editField;
+	}
+	public Double getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Double quantity) {
+		this.quantity = quantity;
+	}
+	public Double getSubAmount() {
+		return subAmount;
+	}
+	public void setSubAmount(Double subAmount) {
+		this.subAmount = subAmount;
+	}
+	public Double getDiscount() {
+		return discount;
+	}
+	public void setDiscount(Double discount) {
+		this.discount = discount;
+	}
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+
 }
